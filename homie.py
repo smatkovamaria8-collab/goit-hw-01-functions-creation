41d238ad98bd2d66d8445b603c5be74b03768b2d
smatkovamaria8-collab <smatkovamaria8@gmail.com>
2025-12-29 22:57:07 +0200

    new changes to exercise 1

--- homie.py
+++ homie.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 
 def get_days_from_today(date):
-    date = datetime.strptime(date, "%Y-%m-%d").date()
-    today = datetime.today().date()
-    difference = date - today
-    return difference.days
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        difference = date - today
+        return difference.days
+    except TypeError:
+        print("Please provide your date in this format: 'YYYY-MM-DD'")
 
-print(get_days_from_today('2025-12-20'))
+print(get_days_from_today(2025-12-20))
